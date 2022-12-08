a63be52cf6c621820df7a95855e922a40cff3dad
Salvatore Santagati <github@salsan.dev>
2022-12-08 19:58:33 +0100

    style: uniform code with ESlint style

--- js/app.js
+++ js/app.js
@@ -1,52 +1,43 @@
-(() => {
-    const txtLng = document.querySelector('p.ask');
-    txtLng.setAttribute('lang', navigator.language);
-})();
+(function () {
+  const txtLng = document.querySelector('p.ask')
+  txtLng.setAttribute('lang', navigator.language)
+}())
 
+addEventListener('load', function () {
+  const originalTxt = document.querySelector('p.ask')
+  const currentTxt = window.getComputedStyle(originalTxt, ':after').content
 
-addEventListener('load', (event) => {
-    const originalTxt = document.querySelector('p.ask');
-    const currentTxt = window.getComputedStyle(originalTxt, ':after').content;
+  translation(currentTxt, getLanguage())
+    .catch(console.error)
+    .then(function (txt) {
+      const styles = document.createElement('style')
+      styles.innerText = `p.ask:lang(${getLanguage()})::after{ content :"${txt[0][0][0].replaceAll('"', '')}";}`
+      document.head.append(styles)
+    })
+})
 
-    const translate = Translation(currentTxt, GetLanguage())
-        .catch(console.error)
-        .then((txt) => {
-            let styles = document.createElement("style");
-            styles.innerText = `p.ask:lang(${GetLanguage()})::after { content :"${txt[0][0][0].replaceAll("\"", '')}";}`;
-            document.head.append(styles);
-        });
-});
+async function translation (words, lng) {
+  // Based on Google Translate API
+  // https://www.labnol.org/code/19909-google-translate-api
 
+  const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${lng}&dt=t&q=${encodeURI(words)}`
 
-async function Translation(words, lng) {
-    // Based on Google Translate API
-    // https://www.labnol.org/code/19909-google-translate-api
+  const options = {
+    method: 'GET'
+  }
 
-    let url = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=en' +
-        '&tl=' +
-        lng +
-        '&dt=t&q=' +
-        encodeURI(words);
-
-    const options = {
-        method: 'GET'
-    };
-
-    return (await fetch(url, options)
-        .then(response => response.json())
-        .catch(err => console.log(err))
-    )
-
+  return (await fetch(url, options)
+    .then((response) => response.json())
+    .catch((err) => console.log(err))
+  )
 }
 
-function GetLanguage() {
-    const regex = /-\w{2}$/gm;
+function getLanguage () {
+  const regex = /-\w{2}$/gm
 
-    const str = navigator.language;
-    const subst = ``;
+  const str = navigator.language
 
-    const result = str.replace(regex, subst);
+  const result = str.replace(regex, '')
 
-    return result;
+  return result
 }
-
